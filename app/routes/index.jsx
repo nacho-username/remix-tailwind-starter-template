edd2697921d4665ddfc3b15552ad1d25127f3bf5
Murray Williams <murray@waterfalldigital.co.za>
2022-12-20 08:47:22 +0200

    Initial commit

---
+++ app/routes/index.jsx
@@ -0,0 +1,12 @@
+export default function Index() {
+  return (
+    <div className='w-full text-center mt-8'>
+      <h1 className='text-3xl font-bold underline'>
+        Welcome to Remix with Tailwind
+      </h1>
+      <p className='text-base tracking-wide'>
+        A starter template with Remix JS and TailwindCSS
+      </p>
+    </div>
+  )
+}
